2c96953fc28586146798fbc9b08c0bc91670f44a
Paul Junod <donujp@gmail.com>
2013-12-19 17:54:34 -0500

    Added header info / little cleanup

--- mergeerrata.py
+++ mergeerrata.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+# Copyright (C) 2013  Paul Junod (donujp@gmail.com)
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+# File Name : mergeerrata.py
+# Creation Date : 
+# Created By : Paul Junod
+# Last Modified : 
+# Purpose : Enable cloned RHN Satellite channels to be syncronized from cron
 
 import getpass
 import socket
@@ -237,8 +259,6 @@ def main():
     else:
         login = options.username
         serverfqdn = options.serverfqdn
-#        origin = options.origin
-#        destination = options.destination
         beginning = options.beginning
 	if options.end == "lastmonth":
 	    end = str(datetime.date.today()+relativedelta(months=-1))
